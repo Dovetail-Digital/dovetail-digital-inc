cb3e1d3d28d65d7ded28e94ec259e8c879a78d7c
Tyler Marshall <tyler@dovetaildigital.ca>
2023-06-06 15:19:54 -0700

    Update pics

--- src/app/components/MainHero.tsx
+++ src/app/components/MainHero.tsx
@@ -81,7 +81,7 @@ export default function MainHero({
             </Button>
           </div>
         </div>
-        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-8">
+        <div className="max-lg:mt-16 md:ml-16">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none max-h-[35rem] overflow-hidden rounded-md">
             <Image
               src={
@@ -94,6 +94,7 @@ export default function MainHero({
               height={"1211"}
               priority={true}
               placeholder="blur"
+              blurDataURL="/hp-pic.png"
             />
           </div>
         </div>
