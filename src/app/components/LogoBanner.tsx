cb2cf04dff90983eb9eeaab0826941b49a266e37
Tyler Marshall <tyler@dovetaildigital.ca>
2023-07-03 18:20:36 -0700

    Update logo sizing on banner

--- src/app/components/LogoBanner.tsx
+++ src/app/components/LogoBanner.tsx
@@ -10,7 +10,7 @@ export default function LogoBanner({
   logo: any;
 }) {
   return (
-    <div className="py-12">
+    <div className="py-16">
       <Typography className="sm:hidden" variant="h2" textAlign="center">
         {title}
       </Typography>
@@ -29,15 +29,20 @@ export default function LogoBanner({
           (logo: {
             title: string;
             alt: string;
-            image: { data: [{ attributes: { url: string } }] };
+            image: {
+              data: [
+                { attributes: { url: string; width: number; height: number } }
+              ];
+            };
           }) => {
             return (
               <Image
                 key={logo.title}
                 src={logo.image.data[0].attributes.url}
                 alt={logo.alt}
-                width={160}
-                height={60}
+                width={logo.image.data[0].attributes.width}
+                height={logo.image.data[0].attributes.height}
+                style={{ height: "auto", maxWidth: 100 }}
               />
             );
           }
