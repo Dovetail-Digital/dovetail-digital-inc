949b01fe1a9d43d0313092d2d72d3c2a4a13f768
Tyler Marshall <tyler@dovetaildigital.ca>
2023-07-08 11:35:10 -0700

    Update iwt

--- src/app/components/ImageWithText.tsx
+++ src/app/components/ImageWithText.tsx
@@ -46,7 +46,7 @@ export default function ImageWithText({
         height={props.item.attributes.height}
         alt={props.item.attributes.alternativeText}
         style={{
-          maxHeight: "800px",
+          maxHeight: "400px",
           objectFit: "contain",
           maxWidth: "100%",
           height: "auto",
